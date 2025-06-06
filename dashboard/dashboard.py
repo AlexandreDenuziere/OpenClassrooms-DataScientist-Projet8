e82376f483fcb8b4131d3cef98958b6834b9997c
Alexandre DENUZIERE <alexandre.denuziere@gmail.com>
2025-06-06 19:04:17 +0000

    Finalisation de l'onglet scoring avec explications

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,10 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+import pickle
+import shap
 
 # Page config
 st.set_page_config(page_title="Customer dashboard", layout="wide")
 
+### ACCESSIBILITY OPTIONS ###
 # Initialize session_state values
 if "show_accessibility" not in st.session_state:
     st.session_state.show_accessibility = False
@@ -47,15 +50,12 @@ st.markdown(f"""
 	}}
     </style>
 """, unsafe_allow_html=True)
+### ACCESSIBILITY OPTIONS ###
 
 # Print the title of the page
 st.title("Prêt à dépenser - Customer dashboard")
 
-# Set data path
-DATA_URL= ("/data/cleaned_data/test_data_cleaned.csv")
-# Set model path
-MODEL_PATH = ("/data/model/model.pkl")
-
+### UTILITY FUNCTIONS ###
 # Load the csv data and put it in cache
 @st.cache_data
 def load_data():
@@ -81,12 +81,73 @@ def get_customer_data(data, customer_id):
     customer_data = customer_data.drop(["DAYS_BIRTH", "DAYS_EMPLOYED"], axis=1)
     return customer_data.iloc[0]
 
+# Load the model
+def load_model():
+    pkl_model = pickle.load(open(MODEL_PATH, "rb"))
+    return pkl_model
+
+# Extract scaler and model  from the pkl model
+def load_scaler_model(pkl_model):
+    scaler = pkl_model.named_steps["scaler"]
+    model = pkl_model.named_steps["model"]
+    return scaler, model
+
+# Compute feature importance
+def get_shap_values(model, scaler):
+    # Scale the data
+    data_transformed = scaler.transform(data)
+    # Calculate the shap values
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(data)
+    return shap_values
+
+# Compute global feature importance
+def get_global_feature_importance(shap_values):
+    # First we get all values in absolute
+    shap_values_abs = np.abs(shap_values)
+    # Then when calculate the mean for each features
+    global_feature_importance = np.mean(shap_values_abs, axis=0)
+    global_feature_importance = pd.DataFrame(
+        global_feature_importance,
+        columns=["Global Importance"],
+        index=data.columns
+    ).reset_index(names="Feature")
+    return global_feature_importance
+
+# Return the local feature importance for a customer
+def get_local_feature_importance(shap_values, customer_index):
+    local_feature_importance = shap_values[customer_index]
+    local_feature_importance = pd.DataFrame(
+        local_feature_importance,
+        columns=["Local Importance"],
+        index=data.columns
+    ).reset_index(names="Feature")
+    return local_feature_importance
+
+### UTILITY FUNCTIONS ###
+
+### DATA LOADING ###
+# Set data path
+DATA_URL = ("/data/cleaned_data/test_data_cleaned.csv")
+# Set model path
+MODEL_PATH = ("/data/model/model.pkl")
+
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load data into the dataframe.
 data = load_data()
+# Load the model
+pkl_model = load_model()
+# Extract scaler and model
+scaler, model = load_scaler_model(pkl_model)
+# Get shap values
+shap_values = get_shap_values(model, scaler)
+# Get the global feature importance
+global_feature_importance = get_global_feature_importance(shap_values)
+
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
+### DATA LOADING ###
 
 # Create the from where the user have to select the customer ID
 with st.form("form"):
@@ -98,6 +159,8 @@ with st.form("form"):
 if submit:
     # Get customer data
     customer_data = get_customer_data(data, customer_id)
+    customer_index = data.loc[data["SK_ID_CURR"] == customer_id, :].index[0]
+    local_feature_importance = get_local_feature_importance(shap_values, customer_index)
     # Arrange customer data to display it
     if customer_data["CODE_GENDER"] == 0 :
         gender = " - is a **man**."
@@ -118,6 +181,24 @@ if submit:
     else :
         realty = f" - {pronoun} **owns** real estate."
 
+    # Get main features by importance
+    # Get 10 most important positive features
+    positive_important_features = local_feature_importance.loc[
+         local_feature_importance["Local Importance"] > 0, :
+    ].sort_values(by="Local Importance", ascending=False, key=abs).head(10)
+    # Get 10 most important negative features
+    negative_important_features = local_feature_importance.loc[
+         local_feature_importance["Local Importance"] < 0, :
+    ].sort_values(by="Local Importance", ascending=False, key=abs).head(10)
+
+    # Add global importance
+    positive_important_features = positive_important_features.merge(global_feature_importance)
+    negative_important_features = negative_important_features.merge(global_feature_importance)
+
+    # Put everything in absolute value
+    positive_important_features["Local Importance"] = np.abs(positive_important_features["Local Importance"])
+    negative_important_features["Local Importance"] = np.abs(negative_important_features["Local Importance"])
+
     # Get data from the API
     response = requests.get(f"http://api:5001/api/v1/customer?id={customer_id}")
 
@@ -126,7 +207,7 @@ if submit:
         response_data = response.json()
 
 	# Create different tabs to organize the application
-        tab1, tab2, tab3, tab4 = st.tabs(["Customer data", "Credit response", "Score explanations", "Toto"])
+        tab1, tab2, tab3, tab4 = st.tabs(["Customer data", "Credit response", "Analyze a feature", "Analyze two features"])
 
 	# In the first tab, we display the customer data
         with tab1:
@@ -144,10 +225,33 @@ if submit:
             st.header("Credit response")
             score = response_data["Probability of the customer being a good one"]
             st.progress(score, f"Probability of the customer being a good one : {np.round(score*100, 1)} %")
+            st.markdown("######")
 
-            
-	# In the third tab 
+            with st.expander("See score explanations"):
+                st.subheader("Most positive features")
+                st.dataframe(
+                   positive_important_features,
+                   hide_index=True
+                )
+                st.subheader("Most negative features")
+                st.dataframe(
+                    negative_important_features,
+                    hide_index=True
+                )
+
+                # Add explanations about table displayed
+                st.markdown("""
+                **Notes :**
+                - The above tables are displaying which features of the customer are the most impacting its score.
+                - **Local importance** are the score obtained by the customer.
+                - It is compared to the **global importance** which is the mean of the score of every customers.
+                """)
+
+	# In the third tab
         with tab3:
-            st.header("Score explanations")
+            st.header("Analyze a feature")
+            selected_feature = st.selectbox("Select a feature to analyze", data.columns)
+        with tab4:
+            st.header("Analyze two features")
     else :
         st.error("Invalid customer ID or error from API")
